d116c010275de2c7b710b59dc6e9cb344a7aeb56
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-11 20:27:58 +0900

    first commit

---
+++ src/atoms/notesAtom.ts
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+/**
+ * ノートのデータ管理場所です。
+ */
+export const notesAtom = atom({
+  key: "Notepad", // 任意のユニークな名前
+  // 初期値
+  default: [
+    {
+      id: "1",
+      value: "",
+      isComplete: false,
+    },
+  ],
+});
