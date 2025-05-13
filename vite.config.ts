2d5e982516ca79425d08a6cb4164d7219a20181d
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-13 10:54:32 +0900

    modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 生成先 ../docs に変更
+  // 生成先 dist に変更
   build: {
     outDir: "dist",
     // 生成先を空にする
